0fce096b5b7aaab7d7db1a7caaa3f8456924bb5c
Bunsinhtor <b.pech.sc.kh@gmail.com>
2019-10-17 12:05:39 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get  'static_pages/about'
-
-  get 'static_pages/contact'
+  get  '/helf',    to: 'static_pages#help', as: 'help'
+  get  '/about',   to: 'static_pages#about', as: 'about'
+  get  '/contact', to: 'static_pages#contact', as: 'contact'
+  get  '/signup',  to: 'users#new'
 
 end
