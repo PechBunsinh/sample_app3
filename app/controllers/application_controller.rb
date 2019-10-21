c4a83122ddbf5c52d803e0544b58578e09c6d8fb
Bunsinhtor <b.pech.sc.kh@gmail.com>
2019-10-21 08:30:03 +0700

    implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "Hello My Friends 99999"  
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/helf',    to: 'static_pages#help', as: 'help'
   get  '/about',   to: 'static_pages#about', as: 'about'
   get  '/contact', to: 'static_pages#contact', as: 'contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 end
